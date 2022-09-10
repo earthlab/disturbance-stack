7a522806b7f76ee53982472ab36fe1e56be0d5a8
Tyler McIntosh <tylermcintosh7@gmail.com>
2022-09-09 18:55:01 -0600

    Finished all data loading for climate analysis

--- workflow/03_ClimateDisturbanceDataAnalysis.R
+++ workflow/03_ClimateDisturbanceDataAnalysis.R
@@ -5,6 +5,17 @@
 #Data inputs for this script were wrangled in GEE,
 #copy of script found in RProject workflow document 02_ClimateDataWrangling.js.
 
+# Analysis per Hammond et al 2022:
+# "For each climate variable (TMAX, VPD, CWD, SOIL M, PPT, PDSI, we calculated the anomaly 
+# (for example, TMAX of the warmest month in the year of mortality - TMAX average for the 
+# 61 values of the same month from 1958–2019). To provide cross-comparison between metrics
+# with different scales and across disparate climates, anomalies were standardized into 
+# zscores, such that time series had a mean of 0 and a standard deviation of 1 based on 
+# using the entire period of record 1958–2019."
+
+# Variable averages for each month across the time period, as well as max/mins/month in 
+# which occurred all part of GEE outputs.
+
 #This script uses the following naming conventions wherever possible:
 # lowerCamelCase for variables
 # period.separated for functions
@@ -39,7 +50,7 @@ monthFileNames <- list.files(path = here("data", "climate", "GEE_terraclimate_pr
 yearVarFileNames <- list.files(path = here("data", "climate", "GEE_terraclimate_prewrangled"), 
                                pattern ="AllVariables*", full.names = TRUE)
 
-#Create list of monthly raster stacks
+#Create SpatRasterDataset of monthly rasters
 monthMeans <- vector(mode = "list", length = 1) #Initialize empty list
 for (file in monthFileNames) {
   monthData <- rast(file)
@@ -48,8 +59,9 @@ for (file in monthFileNames) {
 }
 monthMeans <- monthMeans[-1]
 names(monthMeans) <- month.name
+monthmeans <- sds(monthmeans)  #turn list into spatrasterdataset
 
-#Create list of yearly variable raster stacks
+#Create SpatRasterDataset of yearly variable rasters
 yearVarData <- vector(mode = "list", length = 1) #Initialize empty list
 for (file in yearVarFileNames) {
   yearDats <- rast(file)
@@ -58,8 +70,13 @@ for (file in yearVarFileNames) {
 }
 yearVarData <- yearVarData[-1]
 names(yearVarData) <- as.character(seq(1958, 2021))
+yearVarData <- sds(yearVarData) #turn list into spatrasterdataset
 
-################ PERFORM ANALYSIS ###############
-#Analysis per Hammond et al 2022
+#Compare rasters of both types to ensure same geometries
+compareGeom(monthMeans$January, yearVarData$`1958`)
 
+################ CALCULATE ANOMALY ###############
+
+#calculate anomaly for precipitation for one year first, test method. Then apply over entire list
+testyear <- yearVarData$`1958`
 
